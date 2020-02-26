c86237fcb24037b612122c433c31f44bb3fe4163
NorskJoe <joe.johnson3909@gmail.com>
2020-02-26 14:32:06 +1100

    Started work on book list filter

---
+++ SimplyBooks.Ui/src/app/books/book-action-bar/book-action-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-action-bar',
+  templateUrl: './book-action-bar.component.html',
+  styleUrls: ['./book-action-bar.component.less']
+})
+export class BookActionBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- SimplyBooks.Ui/src/app/books/books.module.ts
+++ SimplyBooks.Ui/src/app/books/books.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { BookComponent } from './book/book.component';
 import { BookFilterComponent } from './book/book-filter/book-filter.component';
 import { BookListComponent } from './book/book-list/book-list.component';
+import { BookActionBarComponent } from './book-action-bar/book-action-bar.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-    declarations: [BookComponent, BookFilterComponent, BookListComponent],
-    imports: [
-        CommonModule
-    ],
-    exports: [
+    declarations: [
+        BookComponent,
         BookFilterComponent,
-        BookListComponent
+        BookListComponent,
+        BookActionBarComponent
+    ],
+    imports: [
+        SharedModule
     ]
 })
 export class BooksModule { }
